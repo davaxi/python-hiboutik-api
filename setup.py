6de40ac02e77061fe6cbcb673cd8c12c27705a62
David Patiashvili <stratagem.david@gmail.com>
2022-08-04 17:42:20 +0200

    Create first complete version of project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hiboutik",
+    version="1.0",
+    description="Client library for Hiboutik APIs",
+    author="David Patiashvili",
+    author_email="stratagem.david+hiboutik@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    license="GPLv3+"
+)
