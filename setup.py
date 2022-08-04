f8eed4df9aad366b6ae6c787514a104241bec06e
David Patiashvili <stratagem.david@gmail.com>
2022-08-04 18:10:03 +0200

    Github CI : Add URL & Readme

--- setup.py
+++ setup.py
@@ -10,5 +10,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
-    license="GPLv3+"
+    license="GPLv3+",
+    readme="README.md",
+    url='https://github.com/davaxi/python-hiboutik-api'
 )
