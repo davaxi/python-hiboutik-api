44123e7fba3963a5aff8854a6bef7dad30b921e7
David Patiashvili <stratagem.david@gmail.com>
2022-08-04 18:11:31 +0200

    Upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hiboutik",
-    version="1.0",
+    version="1.1",
     description="Client library for Hiboutik APIs",
     long_description="Client library for Hiboutik APIs",
     author="David Patiashvili",
